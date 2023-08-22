b398ba5699dffb0c80ab26856c9925eba5ee2551
yuval59 <yuvalmaron222@gmail.com>
2023-08-22 16:14:05 +0300

    Added environment dependency

--- src/env.mjs
+++ src/env.mjs
@@ -2,7 +2,12 @@ import { createEnv } from '@t3-oss/env-nextjs'
 import { z } from 'zod'
 
 export const env = createEnv({
-  server: {},
+  server: {
+    PLANETSCALE_HOST: z.string(),
+    PLANETSCALE_USER: z.string(),
+    PLANETSCALE_PASS: z.string(),
+    PLANETSCALE_DB: z.string(),
+  },
 
   client: {
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
